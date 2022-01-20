ac9f6d6448a1bb328bce4cd297a049f25ede8012
Valentin Cadot <valentin.cadot@gmail.com>
2022-01-20 09:40:09 +0100

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <h1>About Page</h1>
+    <footer>
+      <NuxtLink to="/"> Accueil </NuxtLink>
+    </footer>
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <h1>Hello world !</h1>
+    <NuxtLink to="/about">Home Page</NuxtLink>
+  </main>
+</template>
